b67704b48b545fcc5382dc7e7d25e8c58d275c9d
Samuel Chibueze <sechibueze@gmail.com>
2021-04-30 11:57:23 +0100

    Added update and delete features for users

--- server.js
+++ server.js
@@ -10,18 +10,35 @@ const typeDefs = gql`
     completed: Boolean
   }
 
-  type User {
-    username: String!
-    email: String!
-    password: String!
+  input UserInput {
+    email: String
+    phoneNumber: String
+    emailVerified: Boolean
+    password: String
+    displayName: String
+    photoURL: String
+    disabled: Boolean
   }
-
+  type User {
+    email: String
+    phoneNumber: String
+    emailVerified: Boolean
+    password: String
+    displayName: String
+    photoURL: String
+    disabled: Boolean
+  }
+  type Message {
+    message: String
+  }
   type Query {
     todos: [Todo]
     users: [User]
   }
   type Mutation {
-    signup(username: String!, email: String!, password: String!): User
+    signup(name: String!, email: String!, password: String!): User
+    updateUserById(uid: ID!, fields: UserInput): User
+    deletUserById(uid: String!): Message
   }
 `;
 const todos = [
@@ -47,11 +64,12 @@ const resolvers = {
           if (getUsersResult.notFound.length < 1) {
             return getUsersResult.users.map((userRecord) => ({
               uid: userRecord.uid,
-              username: userRecord.displayName,
+              displayName: userRecord.displayName,
               email: userRecord.email,
               password: userRecord.uid,
               emailVerified: userRecord.emailVerified,
               photoURL: userRecord.photoURL,
+              ...userRecord,
             }));
           } else {
             throw new Error("No user was found");
@@ -64,14 +82,14 @@ const resolvers = {
     },
   },
   Mutation: {
-    signup: (_, { username, email, password }, context) => {
+    signup: (_, { name, email, password }, context) => {
       console.log("TODO:[Mutation.signup]:validate users input", {
-        username,
+        name,
         email,
         password,
       });
       const newUser = {
-        displayName: username,
+        displayName: name,
         email,
         password,
       };
@@ -82,7 +100,7 @@ const resolvers = {
         .then((userRecord) => {
           console.log("[Mutation.signup]:new user record", userRecord);
 
-          return { username, email, password };
+          return { ...userRecord };
         })
         .catch((error) => {
           const err = {
@@ -90,6 +108,41 @@ const resolvers = {
             message: error.message,
           };
           console.log("[Mutation.signup]:error 0", err);
+          throw new Error(error);
+        });
+    },
+    updateUserById: (_, { fields, uid }, context) => {
+      console.log("updateUserById:TODO validate user inputs", fields);
+      /***
+       * Check context: user needs to be authenticated
+       * Or the admin can force this update
+       */
+
+      return admin
+        .auth()
+        .updateUser(uid, fields)
+        .then((userRecord) => {
+          console.log("Update user recors", userRecord);
+          return { ...userRecord };
+        })
+        .catch((error) => {
+          console.info("Failed to update user data", error);
+          throw new Error(error);
+        });
+    },
+    deletUserById: (_, { uid }, context) => {
+      console.log("About to remove user with uid: ", uid);
+      return admin
+        .auth()
+        .deleteUser(uid)
+        .then((result) => {
+          console.log("User successfully removed ", result);
+          return {
+            message: `Goodbye: ${uid} has been removed`,
+          };
+        })
+        .catch((error) => {
+          console.log("Failed to remove user", error);
           throw new Error(error);
         });
     },
