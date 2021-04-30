69b9e1f71beeb83614ce160989cb328f6bd1ba0a
Samuel Chibueze <sechibueze@gmail.com>
2021-04-30 09:10:58 +0100

    Installed and setup firebase admin

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { ApolloServer, gql } = require("apollo-server");
-
+const { config } = require("dotenv");
+config();
+const admin = require("./src/config/firebase-admin");
 const typeDefs = gql`
   type Todo {
     title: String!
@@ -8,8 +10,17 @@ const typeDefs = gql`
     completed: Boolean
   }
 
+  type User {
+    username: String!
+    email: String!
+    password: String!
+  }
+
   type Query {
     todos: [Todo]
+  }
+  type Mutation {
+    signup: User
   }
 `;
 const todos = [
@@ -24,10 +35,29 @@ const resolvers = {
   Query: {
     todos: () => todos,
   },
+  Mutation: {
+    signup: (_, { username, email, password }, context) => {
+      console.log("signup mutation", context);
+      return { username, email, password };
+    },
+  },
 };
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => {
+    const authorization = req.headers.authorization;
+    console.log(" auth", authorization);
+    if (!authorization.startsWith("Bearer ")) {
+      return null;
+    }
+    const token = authorization.split(" ")[1];
+    const decodedToken = admin.auth().verifyIdToken(token);
+
+    console.log("decoded token", decodedToken);
+
+    return decodedToken;
+  },
 });
 
 server
---
+++ src/middlewares/checkUser.js
@@ -0,0 +1,5 @@
+const admin = require("../config/firebase-admin");
+
+function checkUser(req, res, next) {}
+
+module.exports = checkUser;
